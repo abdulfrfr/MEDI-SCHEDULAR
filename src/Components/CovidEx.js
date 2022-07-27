1a2e1e75df75b4d4a1b72f5d54c6e3852c678926
abdulfrfr <abdulmails112@gmail.com>
2022-07-27 14:19:43 +0100

    pushes

--- src/Components/CovidEx.js
+++ src/Components/CovidEx.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 function CovidEx(){
     return(
-        <section className='bg-gray-200 flex-col text-center justify-between items-center px-10 py-5'>
-        <div>
+        <section className='bg-gray-200 xs:flex-col md:flex justify-evenly items-center px-10 py-5'>
+    
         <div className='text-center'>
             <h1 className='text-2xl font-bold mb-3'>COVID-19 SAFETY</h1>
             <p className='w-72 mb-3'>We understand you may have concerns about being exposed to COVID-19. 
@@ -11,24 +11,24 @@ function CovidEx(){
             to the lifesaving work this challenge demands..</p>
             <button className='bg-blue-500 py-3 px-4 mb-4'>SEE MORE</button>
         </div>
-        </div>
-        <div>
+        
+    
         <div className='text-center'>
             <h1 className='text-2xl font-bold mb-3'>COVID-19 SAFETY</h1>
             <p className='w-72 mb-3'>We understand you may have concerns about being exposed to COVID-19. 
             You can trust that our front-line healthcare providers and support staff are dedicated 
             to the lifesaving work this challenge demands..</p>
             <button className='bg-blue-500 py-3 px-4 mb-4'>SEE MORE</button>
+        
         </div>
-        </div>
-        <div>
+    
         <div className='text-center'>
             <h1 className='text-2xl font-bold mb-3'>COVID-19 SAFETY</h1>
             <p className='w-72 mb-3'>We understand you may have concerns about being exposed to COVID-19. 
             You can trust that our front-line healthcare providers and support staff are dedicated 
             to the lifesaving work this challenge demands..</p>
             <button className='bg-blue-500 py-3 px-4 mb-4'>SEE MORE</button>
-        </div>
+        
         </div>
         </section>
     )
